91399423f9bd5f8852cd9ea00a5e661288547708
1-hee <onehee9710@gmail.com>
2025-05-25 18:23:46 +0900

    사용자 로그인 모듈 추가

---
+++ dashboard/src/auth.js
@@ -0,0 +1,21 @@
+// auth.js
+import Cookies from 'js-cookie';
+
+const TOKEN_KEY = 'jwt_token';
+
+export function login(token) {
+  Cookies.set(TOKEN_KEY, token, { expires: 1 }); // 1일 유효
+}
+
+export function logout() {
+  Cookies.remove(TOKEN_KEY);
+  window.location.reload();
+}
+
+export function isAuthenticated() {
+  return !!Cookies.get(TOKEN_KEY);
+}
+
+export function getToken() {
+  return Cookies.get(TOKEN_KEY);
+}
